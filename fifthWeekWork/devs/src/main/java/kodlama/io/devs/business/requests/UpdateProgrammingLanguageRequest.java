ed7fe2849e5e4835ad8f699789b960f3fa121f28
Nurgül <113512193+nurgulcengel@users.noreply.github.com>
2022-11-11 23:36:02 +0300

    javaCamp2022
    javaCamp2022

---
+++ fifthWeekWork/devs/src/main/java/kodlama/io/devs/business/requests/UpdateProgrammingLanguageRequest.java
@@ -0,0 +1,34 @@
+package kodlama.io.devs.business.requests;
+
+public class UpdateProgrammingLanguageRequest {
+
+	public UpdateProgrammingLanguageRequest() {
+		
+	}
+
+	public UpdateProgrammingLanguageRequest(int id, String name) {
+		
+		this.id = id;
+		this.name = name;
+	}
+
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ fifthWeekWork/devs/src/main/java/kodlama/io/devs/dataAccess/abstracts/ProgrammingLanguageRepository.java
@@ -0,0 +1,10 @@
+package kodlama.io.devs.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlama.io.devs.entities.concretes.ProgrammingLanguage;
+
+public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Integer> {
+
+	
+}
